18fb2f82870c93312b3fa5bac91a011f68365cb6
Mathis Favier <favier.mathis@gmail.com>
2020-07-21 14:45:59 +0200

    field and constructor of Malts class

--- src/xyz/louscars/Malts.java
+++ src/xyz/louscars/Malts.java
@@ -4,4 +4,19 @@ public class Malts {
     private int ebc;
     private int lovibond;
     private int potentiel;
+    private String type;
+    private static final String[] TYPE_POSSIBLE = {"grain","extrait","sucre","auxiliaire","autre"};
+
+    public Malts(int ebc, int lovibond, int potentiel, String type){
+        this.ebc = ebc;
+        this.lovibond = lovibond;
+        this.potentiel = potentiel;
+
+        for (String item : TYPE_POSSIBLE) {
+            if (item.trim().equalsIgnoreCase(type)) {
+                this.type = type;
+            }
+        }
+    }
+
 }
