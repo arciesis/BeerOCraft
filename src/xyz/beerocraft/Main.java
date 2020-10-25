f3b2f7179b662fab65cda4a2bac5b1526d08113e
Mathis Favier <favier.mathis@gmail.com>
2020-10-25 19:44:28 +0100

    ajout d'eun preloader pour l'application, pas testé

--- src/xyz/beerocraft/Main.java
+++ src/xyz/beerocraft/Main.java
@@ -1,15 +1,12 @@
-package xyz.beerocraft.model;
+package xyz.beerocraft;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import xyz.beerocraft.model.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.*;
-import java.util.Properties;
 
 public class Main extends Application {
 
@@ -25,13 +22,10 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        Malt m1 = new Malt("testeur 1.0",6,12,70,"grain");
-        Hop h1 = new Hop("test", 15, "cones");
-        Yeast y1 = new Yeast("testun",22,26,70);
-        ConsumableOld c = new ConsumableOld();
-        c.addMalts(m1);
-        c.addYeast(y1);
-        c.addHops(h1);
+        launch(args);
+        Malt m1 = new Malt("test",6,12,70,"grain");
+        Hop h1 = new Hop("testun", 15, "cones");
+        Yeast y1 = new Yeast("testeur",22,26,70);
 
         Consumable consumable = new Consumable();
         consumable.addMaltToBD(m1);
@@ -39,7 +33,6 @@ public class Main extends Application {
         consumable.addYeastToDB(y1);
         
 
-        launch(args);
 
 
     }
--- src/xyz/beerocraft/model/Malt.java
+++ src/xyz/beerocraft/model/Malt.java
@@ -6,31 +6,27 @@ import java.util.Objects;
 
 public class Malt implements Serializable {
 
-    private  static  final  long serialVersionUID = 18273654748474L;
+    private static final long serialVersionUID = 18273654748474L;
     private String name;
     private int ebc;
     private int lovibond;
     private int potential;
     private String type;
-    private static final String[] TYPE_POSSIBLE = {"grain","extrait","sucre","auxiliaire","autre"};
+    private static final String[] TYPE_POSSIBLE = {"grain", "extrait", "sucre", "auxiliaire", "autre"};
 
-    public Malt(String name, int ebc, int lovibond, int potential, String type){
-        if (name.length() < 24) {
-            this.name = name;
-            this.ebc = ebc;
-            this.lovibond = lovibond;
-            if (potential > 0 && potential <= 100)
-                this.potential = potential;
+    public Malt(String name, int ebc, int lovibond, int potential, String type) {
+        this.name = name;
+        this.ebc = ebc;
+        this.lovibond = lovibond;
+        if (potential > 0 && potential <= 100)
+            this.potential = potential;
 
-            for (String item : TYPE_POSSIBLE) {
-                if (item.trim().equalsIgnoreCase(type)) {
-                    this.type = type;
-                }
+        for (String item : TYPE_POSSIBLE) {
+            if (item.trim().equalsIgnoreCase(type)) {
+                this.type = type;
             }
-        } else {
-            System.out.println("The name is larger than expected");
-
         }
+
     }
 
     public String getName() {
